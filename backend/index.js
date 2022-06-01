b8faaa4479023279492b0c7b2dd1cf943dccb1a6
khaledHalhouli <khaled.halhouli@gmail.com>
2022-06-02 00:11:37 +0300

    finishing subcategory

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const roleRouter = require("./routes/role");
 const permissionRouter = require("./routes/permission");
 const categoryRouter = require("./routes/category");
 const registerRouter =require("./routes/register")
-
+const sub_categoryRouter=require("./routes/sub_category")
 const app=express()
 app.use(cors())
 app.use(express.json())
@@ -28,6 +28,10 @@ app.use("/category", categoryRouter);
 
 // creat middlleware application ==>handle all requst registerRouter
 app.use("/register",registerRouter)
+
+// create sub_category router
+app.use("/sub_category",sub_categoryRouter)
+
 
 
 
