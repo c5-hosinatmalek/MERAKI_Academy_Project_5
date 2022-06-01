0dd2cdcd641b7f1f4041e49a264bdc393171bd64
khaledHalhouli <khaled.halhouli@gmail.com>
2022-06-01 23:22:50 +0300

    category

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,23 @@ const cors=require("cors")
 const connection=require("./models/db")
 require("dotenv").config()
 const app=express()
+// midlleware
 app.use(cors())
 app.use(express.json())
 const port=process.env.PORT
 
-
-
-
+// router require
 const roleRouter=require("./routes/role");
 const permissionRouter=require("./routes/permission")
+const categoryRouter=require("./routes/category")
 
-
+//? middleware router
 // creat middlleware application ==>handle all requst roleRouter
 app.use("/role",roleRouter);
 // creat middlleware application ==>handle all requst permissionRouter
 app.use("/permission",permissionRouter);
-
+// create category router
+app.use("/category",categoryRouter)
 
 
 
---
+++ backend/routes/category.js
@@ -0,0 +1,12 @@
+const express= require("express")
+const {createNewCategory}=require("../controllers/category")
+
+
+const categoryRouter=express.Router()
+
+
+//Post request
+
+categoryRouter.post("/",createNewCategory)
+
+module.exports=categoryRouter
