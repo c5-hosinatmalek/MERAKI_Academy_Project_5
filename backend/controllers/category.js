9c6cb36f929269f3db4643564cfeb184e26f007f
khaledHalhouli <khaled.halhouli@gmail.com>
2022-06-01 23:24:45 +0300

    category

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,20 +1,16 @@
-const connection=require("../models/db")
+const connection = require("../models/db");
 
+const createNewCategory = (req, res) => {
+  const { category, picUrl } = req.body;
 
-const createNewCategory=(req,res)=>{
-
-const {category,picUrl} =req.body
-
-const query ="INSERT INTO categories(category,picUrl) VALUES(?,?)"
-const data =[category,picUrl]
-connection.query(query,data,(err,result)=>{
-    if(err){
-        return res.json({success:false,err})
+  const query = "INSERT INTO categories(category,picUrl) VALUES(?,?)";
+  const data = [category, picUrl];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.json({ success: false, err });
     }
-    res.status(201).json({success:true,result})
-})
+    res.status(201).json({ success: true, result });
+  });
+};
 
-
-}
-
-module.exports={createNewCategory}
+module.exports = { createNewCategory };
--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,26 @@
-const express=require("express")
-const cors=require("cors")
-const connection=require("./models/db")
-require("dotenv").config()
-const app=express()
+const express = require("express");
+const cors = require("cors");
+const connection = require("./models/db");
+require("dotenv").config();
+const app = express();
 // midlleware
-app.use(cors())
-app.use(express.json())
-const port=process.env.PORT
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT;
 
 // router require
-const roleRouter=require("./routes/role");
-const permissionRouter=require("./routes/permission")
-const categoryRouter=require("./routes/category")
+const roleRouter = require("./routes/role");
+const permissionRouter = require("./routes/permission");
+const categoryRouter = require("./routes/category");
 
 //? middleware router
 // creat middlleware application ==>handle all requst roleRouter
-app.use("/role",roleRouter);
+app.use("/role", roleRouter);
 // creat middlleware application ==>handle all requst permissionRouter
-app.use("/permission",permissionRouter);
+app.use("/permission", permissionRouter);
 // create category router
-app.use("/category",categoryRouter)
+app.use("/category", categoryRouter);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port,()=>{
-console.log(`server on in port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server on in port ${port}`);
+});
