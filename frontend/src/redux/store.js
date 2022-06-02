d5767605e12432c4354a1aa92ee39fbcc52d60e5
B17_Shishani <83888130+MoadBino@users.noreply.github.com>
2022-06-02 20:27:20 +0300

    end catogre

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./reducers/auth";
+import { catorgeSlice } from "./reducers/catogre";
 export default configureStore({
   reducer: {
     auth: authSlice,
-    
+    catogre: catorgeSlice
   },
 });
