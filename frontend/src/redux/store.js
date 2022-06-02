37dffc1071f498426e43e267ab731f84040b2540
B17_Shishani <83888130+MoadBino@users.noreply.github.com>
2022-06-02 20:48:51 +0300

    products redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./reducers/auth";
 import { catorgeSlice } from "./reducers/catogre";
+import { productSlice } from "./reducers/prodact";
 export default configureStore({
   reducer: {
     auth: authSlice,
-    catogre: catorgeSlice
+    catogre: catorgeSlice,
+    product:productSlice,
+    
   },
 });
+
+
