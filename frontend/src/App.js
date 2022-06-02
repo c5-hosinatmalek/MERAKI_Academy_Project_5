44a1ca9cd2ee8dbb51d21710b98cb1e43251de4c
khaledHalhouli <khaled.halhouli@gmail.com>
2022-06-03 01:14:47 +0300

    categorybar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { Route, Routes, Link } from "react-router-dom";
 import REGISTER from "./component/REGISTER";
-
+import {CategoryBar} from "./component/CategoryBar/index"
 
 function App() {
   return (
     <div className="App">
+      <CategoryBar/>
       <Link to="/rigester">rigester</Link>
       <Routes>
         <Route path="/rigester" element={<REGISTER />} />
+        <Route path="/category/:id/products"/>
       </Routes>
     </div>
   );
