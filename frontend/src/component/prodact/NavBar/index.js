a546c268bb5e8c8141907f1f6a984e3b109a628c
khaledHalhouli <khaled.halhouli@gmail.com>
2022-06-02 23:05:06 +0300

    navbar

---
+++ frontend/src/component/prodact/NavBar/index.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+import {BsCart4} from "react-icons/bs"
+import {RiAccountCircleLine} from "react-icons/ri"
+ const NavBar=()=>{
+
+
+
+    return <div>
+        <img/>
+        <p>{RiAccountCircleLine}My Account</p>
+        <p>{BsCart4}</p>
+        <h1>title</h1>
+    </div>
+}
+export {NavBar}
