10b65a4d0ab0b3fb13b30f52055d4abb598142c4
B17_Shishani <83888130+MoadBino@users.noreply.github.com>
2022-06-02 16:53:41 +0300

    iteams for work

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -100,7 +100,7 @@ const getUserCarts = (req, res) => {
 const checkOut = (req, res) => {
   const user_id = req.token.user_id;
   const data = [user_id];
-  const query = `DELETE FROM cart where user_id=?`;
+  const query = `UPDATE cart SET VALUE is_delete=1 WHERE user_id=?`;
   connection.query(query,data,(err,result)=>{
       if (err) {
           res.status(500).json({
