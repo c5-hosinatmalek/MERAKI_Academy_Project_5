259dd39a2cf4251b5614a4032e478b46001da774
B17_Shishani <83888130+MoadBino@users.noreply.github.com>
2022-06-02 21:01:45 +0300

    cart and done from

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,13 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./reducers/auth";
 import { catorgeSlice } from "./reducers/catogre";
 import { productSlice } from "./reducers/prodact";
+import { cartSlice } from "./reducers/cart";
 export default configureStore({
   reducer: {
     auth: authSlice,
     catogre: catorgeSlice,
-    product:productSlice,
-    
+    product: productSlice,
+    cart: cartSlice,
   },
 });
-
-
