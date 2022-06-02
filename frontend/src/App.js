904d6ed15a963b2367b18d55738cf6c7890bfb10
khaledHalhouli <khaled.halhouli@gmail.com>
2022-06-02 22:03:53 +0300

    startfront

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,83 @@
 import "./App.css";
-
+import {Routes,Route} from "react-router-dom"
 function App() {
-  return <div className="App">Welcome APP</div>;
+  return <div className="App">
+<Routes>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* malek */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</Routes>
+  </div>;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 export default App;
