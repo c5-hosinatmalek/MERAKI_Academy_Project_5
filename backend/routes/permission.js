cd31defc6bb071f3a045e79a580f81defbb5a5d9
hosinatmalek <malek.f.hosinat@gmail.com>
2022-06-02 05:52:40 -0700

    creat all user and delet

--- backend/routes/permission.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const creatPermission = require("../controllers/permission");
-
-//creat middlleware router ==>  permissionRouter
-const permissionRouter = express.Router();
-// post==>http://localhost:5000/permission
-permissionRouter.post("/", creatPermission);
-module.exports = permissionRouter;
--- backend/routes/register.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-// creat middllrware router registerRouter
-const registerRouter = express.Router();
-const register = require("../controllers/register");
-
-// post ==> http://localhost:5000/register
-registerRouter.post("/", register);
-
-module.exports = registerRouter;
