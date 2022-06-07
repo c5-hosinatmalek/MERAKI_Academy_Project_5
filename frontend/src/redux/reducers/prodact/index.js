b117dec19a39452de177aca588a71b0d685396e2
khaledHalhouli <khaled.halhouli@gmail.com>
2022-06-07 22:29:15 +0300

    subCategory

--- frontend/src/redux/reducers/prodact/index.js
+++ frontend/src/redux/reducers/prodact/index.js
@@ -2,18 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 export const productSlice=createSlice({
     name:"product",
     initialState:{
-        product:[]
+        product:[],
+        
         
     },
     reducers:{
         getproduct:(state,action)=>{
             state.product=action.payload
+        },
+        // payload => sub_catgoryId
+        getProductbySubCategoryId:(state,action)=>{
+            state.product=state.product.filter((element)=>{
+                console.log(element.subCategory_id,action.payload,element.subCategory_id==action.payload);
+                return element.subCategory_id==action.payload
+            })
         }
     }
 })
 
 
 
-export const { getproduct } = productSlice.actions;
+export const { getproduct,getProductbySubCategoryId } = productSlice.actions;
 
 export default productSlice.reducer;
