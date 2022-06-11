f60831f50f31bf5f6242aded1f4eca50ae61d3ec
hosinatmalek <malek.f.hosinat@gmail.com>
2022-06-11 12:12:30 -0700

    finished broduct used backend

--- backend/controllers/product_used.js
+++ backend/controllers/product_used.js
@@ -10,7 +10,6 @@ const saleOrder = (req, res) => {
     asking_price,
     bank_account,
     phone_number,
-   
   } = req.body;
   const query = `INSERT INTO usedproduct (product_name,category,product_description,url_imj,asking_price,bank_account,phone_number,user_id) VALUES(?,?,?,?,?,?,?,?)`;
   const data = [
@@ -62,156 +61,162 @@ const requstAccept = (req, res) => {
   });
 };
 
-const getAllSaleOrderForUser=(req,res)=>{
-    const user_id=req.token.user_id;
-    const query=`SELECT * FROM usedproduct WHERE user_id=? AND is_deleted=0 AND admission_status=0`;
-    const data=[user_id];
-    connection.query(query,data,(err,result)=>{
-        if(err){
-            res.status(500).json({
-                success:false,
-                messahge:"server error",
-               err:err
-            })
-            return;
-        }
-        res.status(200).json({
-            success:true,
-            message:"all SaleOrder",
-            result:result
-        })
-    })
-}
-
-const getAllSaleOrderForadmin=(req,res)=>{
-    const query='SELECT * FROM usedproduct WHERE is_deleted=0 AND admission_status=0';
-    connection.query(query,(err,result)=>{
-        if(err){
-            res.status(500).json({
-                success:false,
-                message:"server error",
-                err:err
-            })
-            return;
-        }
-        res.status(200).json({
-            success:true,
-            message:"all sale order",
-            result:result
-        })
-    }) 
-}
-
-
- const ApprovedSalesOrderforUser=(req,res)=>{
-   const user_id = req.token.user_id;
-   const query='SELECT * FROM usedproduct WHERE user_id=? AND admission_status=1 AND is_deleted=0'
-   const data=[user_id];
-   connection.query(query,data,(err,result)=>{
-       if(err){
-           res.status(500).json({
-               success:false,
-               message:"error server",
-               err,err
-
-           })
-           return;
-       }
-       res.status(200).json({
-           success:true,
-           message:"all order approved",
-           result:result
-       })
-   })
-}
-
-const ApprovedSalesOrderforAdmin=(req,res)=>{
-  const query='SELECT * FROM usedproduct WHERE admission_status=1 AND is_deleted=0'
-  connection.query(query,(err,result)=>{
-    if(err){
+const getAllSaleOrderForUser = (req, res) => {
+  const user_id = req.token.user_id;
+  const query = `SELECT * FROM usedproduct WHERE user_id=? AND is_deleted=0 AND admission_status=0`;
+  const data = [user_id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
       res.status(500).json({
-        success:false,
-        message:"server error",
-        err:err
-      })
+        success: false,
+        messahge: "server error",
+        err: err,
+      });
       return;
     }
     res.status(200).json({
-      success:true,
-      message:"get all producr Approved",
-      result:result
-    })
-  })
-}
+      success: true,
+      message: "all SaleOrder",
+      result: result,
+    });
+  });
+};
 
-const deleteProductUsed=(req,res)=>{
-  const used_product_id=req.params.id;
-  const query=`UPDATE usedproduct SET is_deleted=1 WHERE used_product_id=?`;
-  const data=[used_product_id];
-  connection.query(query,data,(err,result)=>{
-    if(err){
+const getAllSaleOrderForadmin = (req, res) => {
+  const query =
+    "SELECT * FROM usedproduct WHERE is_deleted=0 AND admission_status=0";
+  connection.query(query, (err, result) => {
+    if (err) {
       res.status(500).json({
-        success:false,
-        message:"server error",
-        err:err
-      })
+        success: false,
+        message: "server error",
+        err: err,
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: "all sale order",
+      result: result,
+    });
+  });
+};
+
+const ApprovedSalesOrderforUser = (req, res) => {
+  const user_id = req.token.user_id;
+  const query =
+    "SELECT * FROM usedproduct WHERE user_id=? AND admission_status=1 AND is_deleted=0";
+  const data = [user_id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        message: "error server",
+        err,
+        err,
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: "all order approved",
+      result: result,
+    });
+  });
+};
+
+const ApprovedSalesOrderforAdmin = (req, res) => {
+  const query =
+    "SELECT * FROM usedproduct WHERE admission_status=1 AND is_deleted=0";
+  connection.query(query, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        message: "server error",
+        err: err,
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: "get all producr Approved",
+      result: result,
+    });
+  });
+};
+
+const deleteProductUsed = (req, res) => {
+  const used_product_id = req.params.id;
+  const query = `UPDATE usedproduct SET is_deleted=1 WHERE used_product_id=?`;
+  const data = [used_product_id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        message: "server error",
+        err: err,
+      });
       console.log(used_product_id);
-      return
+      return;
     }
     res.status(200).json({
-      success:true,
-      message:"delete proudect successfull",
-      result:result
-    })
-  })
-}
+      success: true,
+      message: "delete proudect successfull",
+      result: result,
+    });
+  });
+};
 
-const allproductusedhardware=(req,res)=>{
-  const category=req.params.category
-  const query=`SELECT * FROM usedproduct WHERE  is_deleted=0 AND admission_status=1 AND category=?`
-  const data =[category]
-  connection.query(query,data,(err,result)=>{
-    if(err){
+const allproductusedhardware = (req, res) => {
+  const category = req.params.category;
+  const query = `SELECT * FROM usedproduct WHERE  is_deleted=0 AND admission_status=1 AND category=?`;
+  const data = [category];
+  connection.query(query, data, (err, result) => {
+    if (err) {
       res.status(500).json({
-        success:false,
-        message:"erorr server",
-        err:err
-      })
-      return
+        success: false,
+        message: "erorr server",
+        err: err,
+      });
+      return;
     }
     res.status(200).json({
-      success:true,
-      message:"all product used from category hardware",
-      result:result
-    })
-  })
-}
+      success: true,
+      message: "all product used from category hardware",
+      result: result,
+    });
+  });
+};
 
-const getOneProdectused=(req,res)=>{
-  const used_product_id=req.params.id
-  const query=`SELECT * FROM usedproduct WHERE used_product_id=?`;
-  const data =[used_product_id];
-  connection.query(query,data,(err,result)=>{
-    if(err){
+const getOneProdectused = (req, res) => {
+  const used_product_id = req.params.id;
+  const query = `SELECT * FROM usedproduct WHERE used_product_id=?`;
+  const data = [used_product_id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
       res.status(500).json({
-        success:false,
-        message:"server error ",
-        err:err
-      })
-      return
+        success: false,
+        message: "server error ",
+        err: err,
+      });
+      return;
     }
     res.status(200).json({
-      success:true,
-      message:"product details",
-      result:result
-    })
-  })
-}
+      success: true,
+      message: "product details",
+      result: result,
+    });
+  });
+};
 
-
-
-
-
-
-
-module.exports = { saleOrder, requstAccept ,getAllSaleOrderForUser,getAllSaleOrderForadmin,ApprovedSalesOrderforUser,ApprovedSalesOrderforAdmin,deleteProductUsed,allproductusedhardware,getOneProdectused};
+module.exports = {
+  saleOrder,
+  requstAccept,
+  getAllSaleOrderForUser,
+  getAllSaleOrderForadmin,
+  ApprovedSalesOrderforUser,
+  ApprovedSalesOrderforAdmin,
+  deleteProductUsed,
+  allproductusedhardware,
+  getOneProdectused,
+};
--- frontend/src/component/LOGIN/index.js
+++ frontend/src/component/LOGIN/index.js
@@ -89,6 +89,7 @@ const LOGIN = () => {
         }
       });
   };
+
   const onfailure = (res) => {
     console.log(res);
   };
