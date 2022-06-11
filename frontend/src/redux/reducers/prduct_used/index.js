13766417a1f1a6ee28f945e4f42449f524402215
khaledHalhouli <khaled.halhouli@gmail.com>
2022-06-11 23:42:43 +0300

    mearge malak

---
+++ frontend/src/redux/reducers/prduct_used/index.js
@@ -0,0 +1,51 @@
+
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const product_used=createSlice({
+    name:"product_used",
+    initialState:{
+        allProductusedfromUser:[],
+        allProductusedApprovedFromUser:[],
+        allProductFromAdmin:[],
+        allProductApprovedFromAdmin:[],
+    },
+    reducers:{
+        addOrderSaleFromUser:(state,action)=>{
+                state.allProductusedfromUser.push(action.payload)
+                state.allProductFromAdmin.push(action.payload)
+        },
+
+        SetAllProductUsedFromUser:(state,action)=>{
+            
+            state.allProductusedfromUser=action.payload
+        },
+
+
+        setallProductUsedApprovedFromUser:(state,action)=>{
+            state.allProductusedApprovedFromUser=action.payload
+        },
+
+
+        setallProductFromAdmin:(state,action)=>{
+            state.allProductFromAdmin=action.payload
+        },
+
+        setallProductApprovedFromAdmin:(state,action)=>{
+            state.allProductApprovedFromAdmin=action.payload
+        },
+
+        requestAccept:(state,action)=>{
+            
+            
+            state.allProductusedApprovedFromUser.push(state.allProductFromAdmin.filter((element)=>{
+                return element.used_product_id ===action.payload
+            }))
+
+
+            state.allProductApprovedFromAdmin.push(state.allProductFromAdmin.filter((element)=>{
+                return element.used_product_id ===action.payload
+            }))
+        }
+    }
+})
